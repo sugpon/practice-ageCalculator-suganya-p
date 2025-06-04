510426308e9affa66d806ba2d961630129af55a8
sugpon <suganya.sugu88@gmail.com>
2025-06-03 23:48:24 -0500

    Assignment Completed

--- AgeCalculator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class AgeCalculator {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); //Create an instance called scanner for imput
-        System.out.println("Enter your age: ");
-        
-    }
-}
